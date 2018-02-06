55bff84b4265b5a509248e960983fa4a0f971525
josevi.agullo <josevi.agullo@gmail.com>
2018-02-06 08:38:47 +0000

    Added data validation

--- FirstStepsAzureFunctions/DataValidatorFunction.cs
+++ FirstStepsAzureFunctions/DataValidatorFunction.cs
@@ -1,4 +1,5 @@
 using FirstStepsAzureFunctions.Models;
+using FirstStepsAzureFunctions.Services;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -7,9 +8,24 @@ namespace FirstStepsAzureFunctions
     public static class DataValidatorFunction
     {
         [FunctionName("DataValidatorFunction")]
-		public static void Run([QueueTrigger("pokemonValidatorQueue")]Pokemon myQueueItem, TraceWriter log)
+		public static void Run([QueueTrigger("pokemonValidatorQueue")]Pokemon pokemon, TraceWriter log)
         {
-	        log.Info($"C# Queue trigger function processed");
+	        log.Info($"Validating Pokemon data");
+
+	        var validatorService = new ValidatorService();
+
+	        var isValidPokemon = validatorService.ValidatePokemon(pokemon);
+
+	        if (isValidPokemon)
+	        {
+		        log.Info("Valid Pokemon");
+	        }
+	        else
+	        {
+		        log.Warning("Invalid Pokemon");
+	        }
+
+			log.Info($"Data validator function processed");
         }
 	}
 }
---
+++ FirstStepsAzureFunctions/Services/ValidatorService.cs
@@ -0,0 +1,12 @@
+﻿using FirstStepsAzureFunctions.Models;
+
+namespace FirstStepsAzureFunctions.Services
+{
+    public class ValidatorService
+    {
+	    public bool ValidatePokemon(Pokemon pokemon)
+	    {
+		    return pokemon.Id != null && !string.IsNullOrEmpty(pokemon.Name);
+	    }
+	}
+}
